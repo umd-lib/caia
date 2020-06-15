e8641a6732b2a41b7453265ca6fb90f57c8da82d
David P. Steelman <dsteelma@umd.edu>
2020-06-15 09:41:31 -0400

    LIBITD-1664. pycodestyle fix
    https://issues.umd.edu/browse/LIBITD-1664

--- caia/circrequests/circrequests_job_config.py
+++ caia/circrequests/circrequests_job_config.py
@@ -55,6 +55,6 @@ class CircrequestsJobConfig(JobConfig):
             denied_keys_filepath = self['denied_keys_filepath']
             if not os.path.exists(denied_keys_filepath):
                 logger.warning(f"denied_keys_filepath file at '{denied_keys_filepath} was not found. Creating default.")
-                denied_keys:List[str] = []
+                denied_keys: List[str] = []
                 with open(denied_keys_filepath, "w") as fp:
                     json.dump(denied_keys, fp)
