1996703d5aac872585c7c92b75f20cd1453b9921
David P. Steelman <dsteelma@umd.edu>
2020-06-25 10:51:00 -0400

    LIBITD-1671. Modified "items" to run multiple src/dest iterations
    Modified the "items" command to run multiple source/destination
    iterations, depending on whether the source returns a "nextitem"
    parameter.
    
    Each iteration will have a separate set of artifacts, distinguished
    by its iteration number, which starts at 1.
    
    https://issues.umd.edu/browse/LIBITD-1671

--- caia/items/source_items.py
+++ caia/items/source_items.py
@@ -1,16 +1,17 @@
-from typing import List, Dict
+from typing import List, Dict, Optional
 
 
 class SourceItems:
     """
     Encapsulates an "items" source response
     """
-    def __init__(self, new_items: List[Dict[str, str]], updated_items: List[Dict[str, str]]):
+    def __init__(self, new_items: List[Dict[str, str]], updated_items: List[Dict[str, str]], next_item: Optional[int]):
         """
         Creates a SourceItems object from the given source response.
         """
         self.new_items = new_items
         self.updated_items = updated_items
+        self.next_item = next_item
 
     def get_new_items(self) -> List[Dict[str, str]]:
         """
@@ -24,9 +25,17 @@ class SourceItems:
         """
         return self.updated_items
 
+    def get_next_item(self) -> Optional[int]:
+        """
+        Returns the index of the next item to request from the source, or None
+        if there are no more items
+        """
+        return self.next_item
+
     def __str__(self) -> str:
         """
         Returns a string representation of this object.
         """
         fullname = f"{self.__class__.__module__}.{self.__class__.__name__}"
-        return f"{fullname}@{id(self)}[new_items: {self.new_items}, updated_items: {self.updated_items}]"
+        return f"{fullname}@{id(self)}[new_items: {self.new_items}, updated_items: {self.updated_items}," \
+               f" next_item: {self.next_item}]"
